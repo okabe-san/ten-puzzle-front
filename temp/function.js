40e538fd16758e2db31394b4b6612c79867834e8
Akiko Okabe <aki0u0@gmail.com>
2016-11-19 16:22:42 -0700

    add 2 dividers cal function

--- temp/function.js
+++ temp/function.js
@@ -30,8 +30,6 @@ function base(num, operators) {
 // input "d" >>> divider
 function typeOne(num, operators) {
   let result = 0;
-  let first = 0;
-  let secont = 0;
   let index = num.indexOf('d');
 
   // new number arrays
@@ -50,36 +48,41 @@ function typeOne(num, operators) {
   return result;
 }
 
-// a, (b, ,c), d >>> two divider
+// a, (b, ,c), d >>> two dividers
 // input "d" >>> divider
-// function typeTwo(num, operators) {
-//   let result = 0;
-//   let first = 0;
-//   let secont = 0;
-//   let third = 0;
-//   let index = num.indexOf('d');
-//   let indexSecond = num.indexOf('d', num.indexOf('d') + 1);
-//
-//   // new number arrays
-//   let numOne = num.slice(0, index);
-//   let numTwo = num.slice(index+1, indexSecond);
-//   let numThree = [];
-//   let numFour = num.slice(indexSecond+1);
-//   let numFive = [];
-//
-//   // new operator arrays
-//   let numOneOperators = plus.concat(operators.slice(0, index));
-//   let numTwoOperators = plus.concat(operators.slice(index+1, indexSecond));
-//   let numThreeOperators = plus.concat(operators.slice(indexSecond-1));
-//   let numFourOperator = plus.concat(operators[index]);
-//   let numFiveOperator = plus.concat(operators[indexSecond-1]);
-//
-//   numThree.push(base(numOne, numOneOperators), base(numTwo, numTwoOperators));
-//
-//   numFive.push(base(numThree, numThreeOperators), base(numFour, numFourOperator));
-//
-//   result = base(numFive, numFiveOperator);
-//   return result;
-// }
-console.log(base([1,2,3,4], ['*', '*', '*']));
-console.log(typeOne([1,'d',2,3,4], ['*', '-', '*']));
+function typeTwo(num, operators) {
+  let result = 0;
+  let index = num.indexOf('d');
+  let indexSecond = num.indexOf('d', num.indexOf('d') + 1);
+
+  // new number arrays
+  let numOne = num.slice(0, index);
+  let numTwo = num.slice(index+1, indexSecond);
+  let numThree = [];
+  let numFour = num.slice(indexSecond+1);
+  let numFive = [];
+
+  // new operator arrays
+  let numOneOperators = operators.slice(0, index-1);
+  let numTwoOperators = operators.slice(index, indexSecond-2);
+  let numThreeOperators = operators.slice(index-1, 1);
+  let numFourOperator = operators.slice(indexSecond);
+  let numFiveOperator = [];
+
+  if (indexSecond-1 === operators.length) {
+    numFiveOperator = operators.slice(indexSecond-2);
+  } else {
+    numFiveOperator = operators.slice(indexSecond-2, 1);
+  }
+
+  numThree.push(base(numOne, numOneOperators), base(numTwo, numTwoOperators));
+
+  numFive.push(base(numThree, numThreeOperators), base(numFour, numFourOperator));
+
+  result = base(numFive, numFiveOperator);
+  return result;
+}
+
+// console.log(base([1,2,3,4], ['*', '*', '*']));
+// console.log(typeOne([1,'d',2,3,4], ['*', '-', '*']));
+console.log(typeTwo([1,'d',8,4,'d',4], ['*', '-', '*']));
